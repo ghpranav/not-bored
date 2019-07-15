8ef24fb27b9ba2b498c0b4d37809817e92dd6dba
ghpranav <bedrepranav@gmail.com>
2019-07-15 16:24:22 +0530

    reg: Add searchKey to database upon signup

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -61,6 +61,7 @@ class Auth implements BaseAuth {
       'imageURL':
           'https://firebasestorage.googleapis.com/v0/b/not-bored.appspot.com/o/pro_pics%2Fdefault.jpg?alt=media&token=2b95838b-d7cd-4540-a7a9-f957e7a526ee',
       'isMailVerified': false,
+      'searchKey': profile['fname'][0],
     });
   }
 

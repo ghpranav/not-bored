df5df7b63ab87a4f7bb20e068c435a16e12f3712
Pranav Bedre <bedrepranav@gmail.com>
2019-12-31 20:46:24 +0530

    functions: fix notification

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,45 @@ const fcm = admin.messaging();
 
 export const sendToDevice = functions.firestore
   .document("users/{userId}/req_rec/{frndId}")
-  .onCreate(async (snapshot, context) => {
-    const frnd = snapshot.data;
+  .onCreate(async (_snapshot, context) => {
+    const frndid = context.params.frndId;
+    const userid = context.params.userId;
+
+    const querySnapshot = await db
+      .collection("users")
+      .doc(userid)
+      .collection("tokens")
+      .get();
+
+    const frndSnapshot = await db
+      .collection("users")
+      .doc(frndid)
+      .get();
+
+    const name = frndSnapshot.get("name");
+
+    const tokens = querySnapshot.docs.map(snap => snap.id);
+
+    const payload: admin.messaging.MessagingPayload = {
+      notification: {
+        title: "Friend Request",
+        body: `${name} sent you a friend request`,
+        icon: "your-icon-url",
+        click_action: "FLUTTER_NOTIFICATION_CLICK",
+        sound: "default"
+      },
+      data: {
+        id: "2",
+        frndid: `${frndid}`
+      }
+    };
+
+    return fcm.sendToDevice(tokens, payload);
+  });
+
+export const sendToDevice2 = functions.firestore
+  .document("users/{userId}/{user}/{frndId}")
+  .onCreate(async (_snapshot, context) => {
     const user = context.params.user;
     const frndid = context.params.frndId;
     const userid = context.params.userId;
@@ -28,7 +65,7 @@ export const sendToDevice = functions.firestore
 
     const tokens = querySnapshot.docs.map(snap => snap.id);
 
-    if (frnd && user == userid) {
+    if (user == userid) {
       const payload: admin.messaging.MessagingPayload = {
         notification: {
           title: "New Friend",
@@ -38,33 +75,19 @@ export const sendToDevice = functions.firestore
           sound: "default"
         },
         data: {
-          id: '1',
-          frndid: `${frndid}`
+          id: "1"
         }
       };
 
       return fcm.sendToDevice(tokens, payload);
-    } else {
-      const payload: admin.messaging.MessagingPayload = {
-        notification: {
-          title: "Friend Request",
-          body: `${name} sent you a friend request`,
-          icon: "your-icon-url",
-          click_action: "FLUTTER_NOTIFICATION_CLICK",
-          sound: "default"
-        },
-        data: {
-          id: '2',
-          frndid: `${frndid}`
-        }
-      };
-      return fcm.sendToDevice(tokens, payload);
     }
+
+    return null;
   });
 
 export const nbMsg = functions.firestore
   .document("users/{userId}/nb_msg/{frndId}")
-  .onCreate(async (snapshot, context) => {
+  .onCreate(async (_snapshot, context) => {
     const userid = context.params.userId;
     const frndid = context.params.frndId;
 
@@ -85,7 +108,7 @@ export const nbMsg = functions.firestore
         sound: "default"
       },
       data: {
-        id: '3',
+        id: "3",
         frndid: `${frndid}`
       }
     };

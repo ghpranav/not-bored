f688c1a8eff5f4eed2997de2738959639f13b12a
baluds <baluds1999@gmail.com>
2020-04-04 05:32:12 +0530

    AboutPage:Completed,Gradient added

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -12,12 +12,28 @@ class _AboutPageState extends State<AboutPage> {
 
  Widget _label() {
     return Container(
-        margin: EdgeInsets.only(top: 40, bottom: 20),
+        margin: EdgeInsets.only(top: 0, bottom: 20),
         child: Column(
           children: <Widget>[
             Text(
-              "You ain't bored anymore",
-              style: TextStyle(color: Colors.white, fontSize: 17),
+              "Here's an app unlike any other socio-friendly apps out there, not only let's you connect and chat with your buddies but provides a feature to actually get you to meet up with your friends. Late night hangouts, after party chilling or just assignment meet ups, you can set up a meet with your friends with just a hit of a button! A new, more personalized app for all ages that's main agenda is to drive away boredom and bring people closer",
+              style: TextStyle(color: Colors.white /*Color(0xFFf96327)*/, fontSize: 17),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+      
+          ],
+        ));
+  }
+
+Widget _label1() {
+    return Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20),
+        child: Column(
+          children: <Widget>[
+            Text("NOT BORED Inc",
+              style: TextStyle(color: Colors.black, fontSize: 13, fontWeight: FontWeight.w600),
             ),
             SizedBox(
               height: 20,
@@ -59,17 +75,30 @@ Widget _privacyPolicy() {
                     blurRadius: 5,
                     spreadRadius: 2)
               ],
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Color(0xFFf96327), Color(0xFFf96327)])),
+               gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+                stops: [0.5, 0.8],
+                colors: [
+                   Colors.orange[700],
+                  Color(0xFFf96327),
+                 
+                ],)),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              _label(),
               SizedBox(
                 height: 80,
               ),
+              SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              
               SizedBox(
                 height: 20,
               ),
@@ -77,6 +106,29 @@ Widget _privacyPolicy() {
                 height: 20,
               ),
                _privacyPolicy(),
+            SizedBox(
+                height: 20,
+              ),SizedBox(
+                height: 20,
+              ),              
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              new Image.asset(
+              'lib/assests/logo.png',
+              fit: BoxFit.cover,
+            ),
+              _label1(),
+              SizedBox(
+                height: 20,
+              ),
+               
             ]
           ),
         ),
